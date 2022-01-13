f633440f05a2fb24aadac45d78740365ce4aafcb
KlaudiaWs <klaudia.warmus@student.howest.be>
2022-01-13 11:54:07 +0100

    Check geldigheid qr code

---
+++ script/Camera.js
@@ -0,0 +1,40 @@
+let afspraakJson = [];
+
+function onScanSuccess(decodedText, decodedResult) {
+  console.log(`Code scanned = ${decodedText}`, decodedResult);
+  checkValidity(decodedText);
+}
+
+var html5QrcodeScanner = new Html5QrcodeScanner('qr-reader', { fps: 10, qrbox: 250 });
+html5QrcodeScanner.render(onScanSuccess);
+
+const get = (url) => fetch(url).then((r) => r.json());
+
+const showResult = (queryResponse) => {
+  for (let i = 0; i < queryResponse.length; i++) {
+    afspraakJson += `\n ${queryResponse[i].afspraakId}`;
+  }
+  console.log(afspraakJson);
+};
+
+const getOnlineAPI = async () => {
+  const endPoint = `https://bezoekersapi.azurewebsites.net/api/afspraken`;
+  const response = await get(endPoint);
+  console.log({ response });
+  showResult(response);
+};
+
+const checkValidity = (code) => {
+  console.log(code);
+  console.log(afspraakJson);
+  if (afspraakJson.indexOf(code) >= 0) {
+    console.log('Code geldig');
+  } else {
+    console.log('Code niet geldig');
+  }
+};
+
+document.addEventListener('DOMContentLoaded', function () {
+  console.log('DOM geladen');
+  getOnlineAPI();
+});
--- script/QR-codeValidation.js
+++ script/QR-codeValidation.js
@@ -23,13 +23,3 @@ const showResultGUID = function (jsonObject) {
     //window.location.href = 'fout.html';
   }
 };
-
-function onScanSuccess(decodedText, decodedResult) {
-  console.log(`Code scanned = ${decodedText}`, decodedResult);
-}
-var html5QrcodeScanner = new Html5QrcodeScanner('qr-reader', { fps: 10, qrbox: 250 });
-html5QrcodeScanner.render(onScanSuccess);
-
-document.addEventListener('DOMContentLoaded', function () {
-  console.log('Camera script');
-});
